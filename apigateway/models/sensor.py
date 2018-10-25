e01e698870d4043f5dce5921a591578906ca0a5d
Stephen Poole <stephen@melon.software>
2018-10-25 17:50:51 +0000

    First pass at updated sensors-to-ddb migration

--- apigateway/models/sensor.py
+++ apigateway/models/sensor.py
@@ -1,86 +1,17 @@
-import json
+import boto3
 
-import datetime
-
-from querypostgres import query_postgres
-from models.entity import Entity
-from fathomapi.utils.exceptions import NoSuchEntityException
+from fathomapi.api.config import Config
+from fathomapi.models.dynamodb_entity import DynamodbEntity
 
 
-class Sensor(Entity):
+class Sensor(DynamodbEntity):
+
+    def _get_dynamodb_resource(self):
+        return boto3.resource('dynamodb').Table(Config.get('DYNAMODB_SENSOR_TABLE_NAME'))
 
     def __init__(self, mac_address):
-        super().__init__({'mac_address': mac_address})
+        super().__init__({'mac_address': mac_address.upper()})
 
     @property
     def mac_address(self):
         return self.primary_key['mac_address']
-
-    @staticmethod
-    def schema():
-        with open('schemas/sensor.json', 'r') as f:
-            return json.load(f)
-
-    def get(self):
-        res = query_postgres(
-            """SELECT
-              NULL AS battery_level,
-              created_at AS created_date,
-              firmware_version AS firmware_version,
-              hw_model AS hardware_model,
-              last_user_id AS last_user_id,
-              id AS mac_address,
-              memory_level AS memory_level,
-              updated_at AS updated_date
-            FROM sensors
-            WHERE sensors.id = %s""",
-            [self.mac_address]
-        )
-
-        if len(res) == 0:
-            raise NoSuchEntityException()
-        return res[0]
-
-    def create(self, body):
-        self.validate('PUT', body)
-        res = query_postgres(
-            """INSERT INTO sensors (
-                id,
-                firmware_version,
-                last_user_id,
-                memory_level,
-                hw_model,
-                created_at,
-                updated_at
-                )
-                VALUES (%s, %s, %s, %s, %s, %s, %s)""",
-            [
-                self.mac_address,
-                body['firmware_version'],
-                body.get('last_user_id', None),
-                body.get('memory_level', None),
-                body.get('hardware_model', None),
-                datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            ]
-        )
-        return self.get()
-
-    def patch(self, body):
-        self.validate('PATCH', body)
-        res = query_postgres(
-            """UPDATE sensors SET
-              firmware_version = %s,
-              last_user_id = %s,
-              memory_level = %s,
-              updated_at = %s
-            WHERE sensors.id = %s""",
-            [
-                body['firmware_version'],
-                body.get('last_user_id', None),
-                body.get('memory_level', None),
-                datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                self.mac_address
-            ]
-        )
-        return self.get()
